6fe0f02fe645fc2514ae27f9974a0169c61ffdbb
yitar <facundo.get2000@gmail.com>
2018-04-04 17:43:17 -0300

    clase chequeo usada en el primer ejercicio de practica para la prueba del 13/04

---
+++ Chequeo.py
@@ -0,0 +1,22 @@
+class chequeo(object):
+    peso = None
+    altura = None
+    fecha = None
+
+    def setPeso(self,peso):
+        self.peso = peso
+
+    def getPeso(self):
+        return self.peso
+
+    def setAltura(self,altura):
+        self.altura = altura
+
+    def getAltura(self):
+        return self.altura
+
+    def setFecha(self,fecha):
+        self.fecha = fecha
+
+    def getFecha(self):
+        return self.fecha
